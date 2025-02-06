baf7d599a8de889c8acdc84564973751964420c5
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-02-06 02:48:06 +0200

    Add homepage | design v2

--- src/main.js
+++ src/main.js
@@ -1,19 +1,1 @@
 import 'bootstrap/dist/js/bootstrap.min.js';
-
-// window.addEventListener('load', () => {
-//     window.history.replaceState('','','/',('#')[0]);
-// });
-
-// window.addEventListener('hashchange', () => {
-//     window.history.replaceState('','','/',('#')[0]);
-// });
-
-// window.addEventListener('load', () => {
-//     window.history.pushState('','','/',('#'),[0]);
-//  }); 
-  
-//  window.addEventListener('hashchange', () => {
-//     window.history.pushState('','','/',('#'),[0]);
-     
-//  });
-
